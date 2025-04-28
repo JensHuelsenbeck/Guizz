2f90ca5e5b6b5f4f3a96ac0f100f8b3de1994ef0
JensHuelsenbeck <jens.huelsenbeck@gmail.com>
2025-04-28 09:43:49 +0200

    Anlegen der Ordnerstruktur

---
+++ app/src/main/java/com/example/guizz/ui/AppStart.kt
@@ -0,0 +1,16 @@
+package com.example.guizz.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AppStart(modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+private fun AppStartPreview() {
+    AppStart()
+}
